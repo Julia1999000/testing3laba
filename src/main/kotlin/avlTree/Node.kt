078b659ca1111214abd357988c319c4ee1cb151b
Julia1999000 <jyliu1999@gmail.com>
2020-12-03 19:43:08 +0300

    create ConstructorNodeTest

--- src/main/kotlin/avlTree/Node.kt
+++ src/main/kotlin/avlTree/Node.kt
@@ -1,7 +1,7 @@
 package main.kotlin.avlTree
 
 class Node(var key: Int, var value: Int, var dad: Node?) {
-    var h = 1
+    var height = 1
     var balance = 0
     var left: Node? = null
     var right: Node? = null
--- src/test/AVLTreeTest.kt
+++ src/test/AVLTreeTest.kt
@@ -1,7 +1,21 @@
 package test
 
-import org.junit.jupiter.api.Assertions.*
+import main.kotlin.avlTree.Node
+import org.junit.Assert
+import org.junit.Test
 
 internal class AVLTreeTest {
 
+    @Test
+    fun ConstructorNodeTest() {
+        val node = Node(1, 1, null)
+
+        Assert.assertEquals(node.height.toLong(), 1)
+        Assert.assertEquals(node.balance.toLong(), 0)
+        Assert.assertEquals(node.key.toLong(), 1)
+        Assert.assertEquals(node.value.toLong(), 1)
+        Assert.assertNull(node.left)
+        Assert.assertNull(node.right)
+        Assert.assertNull(node.dad)
+    }
 }
