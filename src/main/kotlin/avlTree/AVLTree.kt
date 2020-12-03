33c380545f4dfa69b97fd8cc2de7558005d133a2
Julia1999000 <jyliu1999@gmail.com>
2020-12-03 22:23:00 +0300

    create fun height

--- src/main/kotlin/avlTree/AVLTree.kt
+++ src/main/kotlin/avlTree/AVLTree.kt
@@ -1,5 +1,14 @@
 package main.kotlin.avlTree
 
+import kotlin.math.max
+
 class AVLTree {
     var root: Node? = null
+
+    fun height(x: Node?, y: Node?) =
+        if (x == null && y == null) 0
+        else if (x == null) y!!.height
+        else if (y == null) x.height
+        else max(x.height, y.height)
+
 }
