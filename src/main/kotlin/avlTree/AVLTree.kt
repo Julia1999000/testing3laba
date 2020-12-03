9655c3d9667648f8a2887574b72a1bdd710613da
Julia1999000 <jyliu1999@gmail.com>
2020-12-03 22:41:31 +0300

    add fun balance

--- src/main/kotlin/avlTree/AVLTree.kt
+++ src/main/kotlin/avlTree/AVLTree.kt
@@ -11,4 +11,10 @@ class AVLTree {
         else if (y == null) x.height
         else max(x.height, y.height)
 
+    fun balance(x: Node?, y: Node?) =
+        if (x == null && y == null) 0
+        else if (x == null) -y!!.height
+        else if (y == null) x.height
+        else x.height - y.height
+
 }
